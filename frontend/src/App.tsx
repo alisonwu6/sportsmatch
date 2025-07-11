040f165fe81531113483e68f5e33f5b3d121278f
Alison Wu <alison.wu23@gmail.com>
2025-07-11 15:49:41 +1000

    chore: npm install --save-dev typescript @types/react @types/react-dom

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-2xl font-bold text-green-400">Hello, SportsMatch</h1>
+    </div>
+  )
+}
+
+export default App
